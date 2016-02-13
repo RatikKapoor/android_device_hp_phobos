dffcd797cb1da04b10de9c5c4f463a4f7326ee4a
Quaesar <quaesarxda@gmail.com>
2016-02-12 21:34:31 -0700

    phobos: Fix some inconsistencies from M bringup

--- device.mk
+++ device.mk
@@ -94,10 +94,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
 
-# Power
-PRODUCT_PACKAGES += \
-    power.phobos
-
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.dalmore \
